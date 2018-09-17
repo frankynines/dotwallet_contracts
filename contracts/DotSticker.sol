dafed20be77fbf69318281d011a1f3752fe6e1ef
Franky Aguilar <franky@99centbrains.com>
2018-09-17 17:08:19 -0400

    Contract Dev

--- contracts/DotSticker.sol
+++ contracts/DotSticker.sol
@@ -4,118 +4,193 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
- * @title DotSticker
+ * @title DotCollectible
  * DotSticker - a contract for Dot Wallet non-fungible Tokens.
  */
-contract DotSticker is ERC721Token, Ownable {
-
-    mapping (uint256 => address) internal contractOwner;
+contract DotCollectible is ERC721Token, Ownable {
     
-    uint internal mintingFee = 0.255 ether;
+    mapping (uint256 => address) internal contractOwner;
+    mapping (uint256 => uint) private tokenEdition;
+    mapping (uint256 => uint256) private tokenOriginator;
+    mapping (uint256 => uint256) private tokenGifter;
+
+    uint private mintingFee = 0.1 ether;
+    uint private giftingFee = 0.1 ether;
+    
+    uint256 private tokenBalance = 0;
+    uint256 private bankBalance = 0;
+    
+     constructor(string _name, string _symbol) 
+        public ERC721Token(_name, _symbol) { 
+
+    }
 
     struct Sticker {
         string title;
-        address creator;
-        uint64 created;
-        uint256 edition;
+        uint generation;
+        uint edition;
+        uint256 value;
     }
 
     Sticker[] public allstickers;
-
-    constructor (string name, string symbol) public
-        ERC721Token(name, symbol) 
-    { }
 
     // ADMIN METHODS
     function setMintingFee(uint256 _price) public onlyOwner{
         mintingFee = _price;
     }
+    
+    function setGiftinFee(uint256 _price) public onlyOwner{
+        giftingFee = _price;
+    }
 
     /* Minting Functions */
-
     /**
     * @dev Mints a token to an address with a tokenURI.
     * @param _to address of the future owner of the token
     * @param _tokenURI token URI for the token
     */
-    function mintCollectible(
+    function mintCollectible (
             string _title,
             address _to, 
             string _tokenURI
             ) public onlyOwner {
         
-        uint64 _birth = uint64(now);
-        uint256 _edition = 0;
+        uint _edition = 0;
+        uint _generation = 0;
         
-        Sticker memory _sticker = Sticker({title:_title, creator: msg.sender, created: _birth, edition: _edition});
+        Sticker memory _sticker = Sticker({
+            title:_title, 
+            generation: _generation,
+            edition: _edition,
+            value: 0
+        });
+        
         uint256 _newTokenId = allstickers.push(_sticker) - 1;
-
+        tokenOriginator[_newTokenId] = _newTokenId; // GEN0
+        tokenGifter[_newTokenId] = _newTokenId;
+        
+        tokenEdition[_newTokenId] = _edition;
+        
         _mint(_to, _newTokenId);
         _setTokenURI(_newTokenId, _tokenURI);
         
     }
     
     /**
-    * @dev Clones a token based off of Edition Number
+    * @dev Mints a user generated token.
+    * @param _to address of the future owner of the token
+    * @param _tokenURI token URI for the token
+    */
+    function mintUGCollectible (
+            string _title,
+            address _to, 
+            string _tokenURI
+            ) public payable {
+                
+        require(msg.value >= mintingFee);
+        
+        uint _edition = 0;
+        uint _generation = 0;
+        
+        Sticker memory _sticker = Sticker({
+            title:_title, 
+            generation: _generation,
+            edition: _edition,
+            value: 0
+        });
+        
+        uint256 _newTokenId = allstickers.push(_sticker) - 1;
+        tokenOriginator[_newTokenId] = _newTokenId;
+        tokenGifter[_newTokenId] = _newTokenId;
+        
+        _mint(_to, _newTokenId);
+        _setTokenURI(_newTokenId, _tokenURI);
+    }
+    
+    /**
+    * @dev Gifts a token based off of Edition and Generation Number
     * @param _to address of the future owner of the token
     * @param _tokenId token ID to clone
     */
-    function cloneCollectible(address _to, uint256 _tokenId) public onlyOwner {
+    function giftCollectible(uint256 _tokenId, address _to) public payable {
         
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+
+        //Get old sticker
         Sticker storage _sticker = allstickers[_tokenId];
+        uint _edition = _sticker.edition + 1;
+        uint _generation = _sticker.generation + 1;
         
-        uint64 _birth = uint64(now);
-        uint256 _edition = _sticker.edition + 1;
+        //If gen 0 do not require gifting fee
+        if (_sticker.generation > 0) {
+            require(msg.value >= giftingFee);
+            msg.sender.transfer(msg.value);
+        }
         
-        string storage _originalTitle = _sticker.title;
-        address _originalCreator = _sticker.creator;
+        tokenEdition[_tokenId] = _edition;
         
-        Sticker memory _newSticker = Sticker({title: _originalTitle, creator: _originalCreator, created: _birth, edition: _edition});
+        Sticker memory _newSticker = Sticker({
+            title:_sticker.title, 
+            generation: _generation,
+            edition: _edition,
+            value: 0
+        });
         
         uint256 _newTokenId = allstickers.push(_newSticker) - 1;
+        
+        //Assign Generation and Gifting
+        updateTokenGenEdition(_tokenId, _newTokenId);
         
         _mint(_to, _newTokenId);
         _setTokenURI(_newTokenId, tokenURI(_tokenId));
     }
-
-    /**
-    * @dev Mints a UserGenerated Collectible with Fee
-    * @param _to address of the future owner of the token
-    * @param _tokenURI token URI for the token
-    */
-
-    function mintUGCollectible(
-            address _to,
-            string _title,
-            string _tokenURI
-        ) public payable {
-
-        //Require Publishing Fee
-        require(msg.value == mintingFee);
-        msg.sender.call.value(msg.value).gas(20317);
-        // address(this).transfer(mintingFee);
+    
+    function updateTokenGenEdition(uint256 _tokenId, uint256 _newTokenId) internal {
         
-        uint64 _birth = uint64(now);
-        uint256 _edition = 0;
+        if (exists(_tokenId) == true) {
+            //Assign Parent Token ID to New Token
+            tokenGifter[_newTokenId] = _tokenId;
+        
+            uint256 _tokenZ = tokenOriginator[_tokenId];
+            updateTokenValue(_tokenZ, msg.value/100);
+        
+            uint256 _tokenX = tokenGifter[_tokenId];
+            updateTokenValue(_tokenX, msg.value/10);
+        }
 
-        Sticker memory _sticker = Sticker({title:_title, creator: msg.sender, created: _birth, edition: _edition});
-        uint256 _newTokenId = allstickers.push(_sticker) - 1;
-
-        _mint(_to, _newTokenId);
-        _setTokenURI(_newTokenId, _tokenURI);
     }
     
-
-    /**
-    * @dev Removes a NFT from owner.
-    * @param _owner Address from wich we want to remove the NFT.
-     * @param _tokenId Which NFT we want to remove.
-    */
-    function burn(
-        address _owner,
-        uint256 _tokenId
-    ) external onlyOwner {
-        super._burn(_owner, _tokenId);
+    function getTokenValue(uint256 _tokenId) public view returns(uint256) {
+        Sticker storage sticker = allstickers[_tokenId];
+        return sticker.value;
+    }
+    
+    function topupTokenValue(uint256 _tokenId) public payable {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        
+        if (msg.value > 0){
+            msg.sender.transfer(msg.value);
+            updateTokenValue(_tokenId, msg.value);
+        }
+    }
+    
+    function updateTokenValue(uint256 _tokenId, uint256 _value) internal {
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        
+        Sticker storage sticker = allstickers[_tokenId];
+        sticker.value += _value;
+        tokenBalance += _value;
+    }
+    
+    function drainTokenValue(uint256 _tokenId) internal {
+        Sticker storage sticker = allstickers[_tokenId];
+        tokenBalance -= sticker.value;
+        sticker.value = 0;
+    }
+   
+    function burnToken(uint256 _tokenId) public {
+        drainTokenValue(_tokenId);
+        _burn(msg.sender, _tokenId);
     }
 
     /**
@@ -139,16 +214,54 @@ contract DotSticker is ERC721Token, Ownable {
         return allTokens;
     }
     
-    function withdraw() public onlyOwner {
-        address(owner).transfer(address(this).balance);
+    /**
+    * @dev Allows user to redeem balance of Token.
+    * @param _tokenId token ID of user
+    */
+    function redeem(uint256 _tokenId) public {
+        
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        
+        uint256 val = getTokenValue(_tokenId);
+        
+        if (val <= tokenBalance) {
+            address(msg.sender).transfer(val);
+            drainTokenValue(_tokenId);
+        }
     }
     
+    /**
+    * @dev Allows onlyOwner to withdraw from contract balance.
+    * @param value value to withrdraw
+    */
+    function withdraw(uint256 value) public onlyOwner {
+        if (value <= contractBankBalance()){
+            address(owner).transfer(value);
+        }
+    }
+
     function contractBalance() public view returns (uint256){
         return address(this).balance;
     }
+    
+    function contractBankBalance() public view returns (uint256){
+        return address(this).balance - tokenBalance;
+    }
         
-    function getMintingFee() public view returns (uint) {
+    function getMintingFee() internal view returns (uint) {
         return mintingFee;
+    }
+    
+    function getGiftinFee() internal view returns (uint) {
+        return giftingFee;
+    }
+    
+    function getStickerGifter(uint256 _tokenId) view public returns (uint) {
+        return tokenGifter[_tokenId];
+    }
+    
+    function getStickerOriginator(uint256 _tokenId) view public returns (uint) {
+        return tokenOriginator[_tokenId];
     }
 
 }
